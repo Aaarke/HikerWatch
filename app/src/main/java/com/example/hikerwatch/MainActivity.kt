e95a87234e4bd3359678119b1fde1ceb3e1859fe
Aaarke <roshankumar01@jungleworks.co>
2019-04-20 19:06:16 +0530

    ui devlopment started

--- app/src/main/java/com/example/hikerwatch/MainActivity.kt
+++ app/src/main/java/com/example/hikerwatch/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.hikerwatch
 
+import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setLocationManager()
+    }
+
+    private fun setLocationManager(){
+        var locationManager:LocationManager=LocationManager()
     }
 }
